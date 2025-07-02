3ee2ce8662e9748843cc88a99e5c8108924c9bbd
Nasadmi <samueldiego828@gmail.com>
2025-07-02 23:54:15 +0200

    Update: restrict mobile devices access

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,50 @@
-import './App.css'
-import { Sidebar } from './components/Sidebar'
-import { Search } from './components/Search'
-import { ViewModeProvider } from './provider/ViewMode.provider'
-import { NotesIdProvider } from './provider/NotesId.provider'
-import { NotesList } from './components/NotesList'
-import { NotesContent } from './components/NotesContent'
-import { NotesManager } from './components/NotesManager'
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Sidebar } from "./components/Sidebar";
+import { Search } from "./components/Search";
+import { ViewModeProvider } from "./provider/ViewMode.provider";
+import { NotesIdProvider } from "./provider/NotesId.provider";
+import { NotesList } from "./components/NotesList";
+import { NotesContent } from "./components/NotesContent";
+import { NotesManager } from "./components/NotesManager";
 
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+    const regex = /android|iphone|ipad|ipod|blackberry|windows phone/i;
+
+    if (regex.test(userAgent)) {
+      setIsMobile(true);
+    }
+  }, []);
 
   return (
-    <div className="layout">
-      <ViewModeProvider>
-        <Sidebar />
-        <main>
-          <Search />
-          <section className='layout-grid relative'>
-            <NotesIdProvider>
-              <NotesList />
-              <NotesContent />
-              <NotesManager />
-            </NotesIdProvider>
-          </section>
-        </main>
-      </ViewModeProvider>
-    </div>
-  )
+    <>
+      {!isMobile ? (
+        <div className="layout">
+          <ViewModeProvider>
+            <Sidebar />
+            <main>
+              <Search />
+              <section className="layout-grid relative">
+                <NotesIdProvider>
+                  <NotesList />
+                  <NotesContent />
+                  <NotesManager />
+                </NotesIdProvider>
+              </section>
+            </main>
+          </ViewModeProvider>
+        </div>
+      ) : (
+        <div className="mobile-layout inter">
+          <h1>Mobile support not available</h1>
+        </div>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
